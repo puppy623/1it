c548dc35665b4ae5ae4a20c73c28996258bad8b5
Radek Gablas <sniickers.san@gmail.com>
2025-01-29 19:41:58 +0000

    Add files via upload

---
+++ #salary calculator.py
@@ -0,0 +1,10 @@
+#salary calculator
+
+salary = 1299.99
+tax = 0.13
+expenses = 100.0
+bonus = 0.1
+print("Salary: ", salary)
+print("Tax: ", salary * tax)
+print("Expenses: ", expenses)
+print("Bonus: ", salary * bonus)
